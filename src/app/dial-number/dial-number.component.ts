3a00fb6a4f71d774d01f560663584e03eed830cf
Martin Grotz <martin.grotz@redheads.de>
2017-11-28 12:08:51 +0100

    Nummernwahl implementiert

--- src/app/dial-number/dial-number.component.ts
+++ src/app/dial-number/dial-number.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dial-number.component.scss']
 })
 export class DialNumberComponent implements OnInit {
+  dialedNumber: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.dialedNumber = '';
   }
 
+  ngOnInit() {}
+
+  onBackspaceClicked() {
+    if (this.dialedNumber.length) {
+      if (this.dialedNumber.length === 1) {
+        this.dialedNumber = '';
+      } else {
+        this.dialedNumber = this.dialedNumber.substring(0, this.dialedNumber.length - 1);
+      }
+    }
+  }
+
+  onNumberClicked(num) {
+    this.dialedNumber += num;
+  }
 }
